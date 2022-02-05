851a90f4acba8cc79273b1afe4e8ba3e89cc73a6
GUsiku <afrogem3@gmail.com>
2022-02-05 12:47:34 +0800

    deleted some import derivatives that were not in use

--- app/src/main/java/com/gemmausiku/thesisnotes/MainActivity.kt
+++ app/src/main/java/com/gemmausiku/thesisnotes/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.gemmausiku.thesisnotes
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+i
 import androidx.navigation.fragment.NavHostFragment
 import com.gemmausiku.thesisnotes.databinding.ActivityMainBinding
 
